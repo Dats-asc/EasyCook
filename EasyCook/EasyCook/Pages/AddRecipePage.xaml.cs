5124dd8203fc724eb14f993ce89d6116376c5d03
AlsuKhairullina <alcukhairullina@yandex.ru>
2021-04-27 15:56:56 +0300

    bruh

---
+++ EasyCook/EasyCook/Pages/AddRecipePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace EasyCook.Pages
+{
+    public partial class AddRecipePage : ContentPage
+    {
+        public AddRecipePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EasyCook/EasyCook/Pages/RecipesListPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace EasyCook.Pages
+{
+    public partial class RecipesListPage : ContentPage
+    {
+        public RecipesListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
